82c484c8d156caea317bf0b1371177f0fa3f9e61
Fraser Murray <fraser.m.murray@gmail.com>
2015-05-06 03:14:33 +0100

    fix chart-gen

--- chart-generator/Main.hs
+++ chart-generator/Main.hs
@@ -1,12 +1,15 @@
 module Main where
 
 import Control.Applicative
-import Data.ByteString.Lazy.Char8 (ByteString)
+import Data.ByteString.Char8 (ByteString)
 import Graphics.Rendering.Chart.Backend.Diagrams
 import System.Environment
 import System.Exit
+import Data.Map (Map)
 import qualified Data.ByteString.Lazy.Char8 as ByteString
 import qualified Data.Csv as CSV
+import qualified Data.Vector as Vector
+import qualified Data.Map as Map
 
 main :: IO ()
 main = getArgs >>= go
@@ -14,9 +17,12 @@ main = getArgs >>= go
 go :: [String] -> IO ()
 go = \case
   filename : xs -> do
-    Right csv <- CSV.decode CSV.HasHeader <$> ByteString.readFile filename
+    Right (headers, csv) <- CSV.decodeByName <$> ByteString.readFile filename
     case xs of
-      _ -> return ()
+      _ -> charts (Vector.toList headers) (Vector.toList csv)
   _ -> do
     putStrLn "Invalid arguments"
     exitFailure
+
+charts :: [ByteString] -> [Map ByteString ByteString] -> IO ()
+charts = undefined
